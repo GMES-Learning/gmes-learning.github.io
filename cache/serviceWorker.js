96e5c8644d062c119103a2fb42aeec4f01d88e3e
GMES-Learning <104249750+GMES-Learning@users.noreply.github.com>
2024-08-17 09:49:39 -0300

    Add files via upload

---
+++ cache/serviceWorker.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('cacheName').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/cache/index.html',
+                '/cache/example.css',
+                '/cache/example.js'
+            ])
+        })
+    )
+})
+
+self.addEventListener('fetch', e => {
+
+    console.log(e.request.url)
+
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    )
+})
