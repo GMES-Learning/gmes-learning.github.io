4bf76cb486b740a7cff4a3d0b454b93f8a978981
GMES-Learning <104249750+GMES-Learning@users.noreply.github.com>
2024-10-27 14:25:46 -0300

    Update serviceWorker.js

--- cache/serviceWorker.js
+++ cache/serviceWorker.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open('cacheName').then(function(cache) {
+        caches.open('cache-name3').then(function(cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
