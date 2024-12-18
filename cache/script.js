e15d4ddfe8b2c2bec65efc58289556cf20dd4912
GMES-Learning <104249750+GMES-Learning@users.noreply.github.com>
2024-12-18 02:21:54 -0300

    Add files via upload

--- cache/script.js
+++ cache/script.js
@@ -3,10 +3,11 @@ var h1 = document.querySelector('h1')
 
 h1.textContent = 'Cache Example'
 
+// Registra o service worker para controlar o funcionamento do site offline:
 
 if('serviceWorker' in navigator) {
 
     navigator.serviceWorker
         .register('/serviceWorker.js')
-        .then(function() { console.log('Service Worker Registrado!') })
+        .then(() => console.log('Service Worker Registrado!'))
 }
