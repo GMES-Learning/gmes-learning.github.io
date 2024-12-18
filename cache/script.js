93618f6fe1cea54208cdb3cdd3c92409b57e0b9e
GMES-Learning <104249750+GMES-Learning@users.noreply.github.com>
2024-12-18 14:57:50 -0300

    Update script.js

--- cache/script.js
+++ cache/script.js
@@ -3,13 +3,6 @@ var h1 = document.querySelector('h1')
 
 h1.textContent = 'Cache Example'
 
-self.addEventListener('install', e => {
-    
-    console.log('ok')
-})
-
-// Registra o service worker para controlar o funcionamento do site offline:
-
 if('serviceWorker' in navigator) {
 
     navigator.serviceWorker
