1852df3f0c20df0a4d66de213bca34346e0513e0
GMES-Learning <104249750+GMES-Learning@users.noreply.github.com>
2025-01-08 17:00:23 -0300

    Add files via upload

--- cache/script.js
+++ cache/script.js
@@ -4,8 +4,18 @@ var h1 = document.querySelector('h1')
 h1.textContent = 'Cache Example'
 
 if('serviceWorker' in navigator) {
+    
+    // Atenção: Um service worker só pode controlar arquivos no mesmo diretório que ele e em subdiretórios.
 
     navigator.serviceWorker
-        .register('/serviceWorker.js')
+        // O caminho do arquivo é relativo à origem do site.
+        .register('/serviceWorker.js') 
         .then(() => console.log('Service Worker Registrado!'))
 }
+
+
+
+h1.addEventListener('click', function(evt) {
+
+    fetch('https://gmes-learning.github.io/dogs.json')
+})
